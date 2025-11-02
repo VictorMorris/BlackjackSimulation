3780526e896e17734e029346f68c82fea6bce69f
Victor Morris <57648247+VictorMorris@users.noreply.github.com>
2025-11-02 12:01:09 -0500

    Updated to keep track of additional stats

--- sim.py
+++ sim.py
@@ -2,7 +2,8 @@ import random
 import matplotlib.pyplot as plt
 
 
-odds = [49, 42, 9]
+ODDS = [49, 42, 9]
+EV = -0.07
 
 def main():
     print("Welcome to blackjack")
@@ -13,22 +14,55 @@ def main():
     bet = int(input())
 
     player_total = 0
-    result = 0
-    result_table = []
-    hands_table = []
+    hand_result = 0
+    result_table_y = []
+    hand_table_x = []
+    player_result = []
+    player_ev = []
+
+    longest_win_streak = 0
+    current_win_streak = 0
+    highest_profit = 0
+    
     for hand in range(hands):
-        result = random.random() * 100
+        hand_result = random.random() * 100
         #0-49
-        if result <= odds[0]:
+        if hand_result <= ODDS[0]:
             player_total -= bet
+            player_result.append(-1)
+            current_win_streak = 0
         #49-91
-        elif odds[0] < result <= odds[0] + odds[1]:
+        elif ODDS[0] < hand_result <= (ODDS[0] + ODDS[1]):
             player_total += bet
+            player_result.append(1)
+            current_win_streak += 1
 
-        result_table.append(player_total)
-        hands_table.append(hand)
+        else:
+            player_result.append(0)
 
-    plt.plot(hands_table, result_table)
+        result_table_y.append(player_total)
+        hand_table_x.append(hand)
+        player_ev.append(EV * bet * hand)
+
+        if result_table_y[-1] > highest_profit:
+            highest_profit = result_table_y[-1]
+
+        if current_win_streak > longest_win_streak:
+            longest_win_streak = current_win_streak
+
+    wins = player_result.count(1)
+    losses = player_result.count(-1)
+    ties = player_result.count(0)
+
+    print(f"""Player wins {wins} for a total win percentage of {wins / (wins + losses + ties) * 100}
+Player losses {losses} for a total loss percentage of {losses / (wins + losses + ties) * 100}
+Player ties {ties} for a total tie percentage of {ties / (wins + losses + ties) * 100}
+Players longest win streak was {longest_win_streak}
+Players highest profit was {highest_profit}
+Player wins a total of {result_table_y[-1]}""")
+
+    plt.plot(hand_table_x, result_table_y)
+    plt.plot(hand_table_x, player_ev)
     plt.xlabel("Hands")
     plt.ylabel("Profit")
     plt.title("Blackjack Results")
